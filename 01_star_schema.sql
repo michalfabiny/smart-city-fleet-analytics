1f71e445f10f940a3c5af60d487deadd1fbd3136
michalfabiny <michal.fabiny@gmail.com>
2026-09-24 18:34:38 +0200

    Add 01_star_schema.sql

---
+++ 01_star_schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS `Smart_City_fleet_management.fakt_jazdy` (
+    Jazda_ID STRING,
+    Datum_Jazdy DATE,
+    Vozidlo_ID STRING,
+    Uzivatel_ID STRING,
+    Trvanie_Minuty INT64,
+    Vzdialenost_KM NUMERIC,
+    Cena_Celkom NUMERIC
+);
